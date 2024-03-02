cd238f109043f7ea1d97c246b905b57e265f9f85
IdirX69 <adil.idirr@gmail.com>
2024-03-02 14:02:21 +0100

    map error

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from "redux";
+import userReducer from "./user.reducer";
+import postReducer from "./post.reducer";
 
-export default combineReducers({});
+export default combineReducers({
+  userReducer,
+  postReducer,
+});
---
+++ src/reducers/user.reducer.js
@@ -0,0 +1,5 @@
+const initialState = { user: "idir idir" };
+
+export default function userReducer(state = initialState, action) {
+  return state;
+}
