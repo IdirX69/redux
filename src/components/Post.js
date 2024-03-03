bc0b939dddf08779e46311546a26b4f5b67d0f82
IdirX69 <adil.idirr@gmail.com>
2024-03-03 16:40:07 +0100

    user post and edit

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Like from "./Like";
 import { isEmpty } from "./Utils";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { editPost, getPosts } from "../actions/post.action";
 
 const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
   const user = useSelector((state) => state.userReducer);
-
+  const form = useRef();
+  const [editContent, setEditContent] = useState(post.content);
+  const dispatch = useDispatch();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const editData = {
+      author: user.pseudo,
+      title: post.title,
+      content: editContent,
+      likes: post.likes,
+      id: post.id,
+    };
+    await dispatch(editPost(editData, post.id));
+    dispatch(getPosts());
+    setEditToggle(false);
+  };
   return (
     <div className="post">
       {!isEmpty(user) && user.pseudo === post.author && (
@@ -28,8 +44,12 @@ const Post = ({ post }) => {
       />
 
       {editToggle ? (
-        <form>
-          <textarea autoFocus={true} defaultValue={post.content}></textarea>
+        <form ref={form} onSubmit={(e) => handleSubmit(e)}>
+          <textarea
+            onChange={(e) => setEditContent(e.target.value)}
+            autoFocus={true}
+            defaultValue={post.content}
+          ></textarea>
           <input type="submit" value="Valider modification" />
         </form>
       ) : (
--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -1,11 +1,19 @@
-import { GET_POSTS } from "../actions/post.action";
+import { GET_POSTS, ADD_POST, EDIT_POST } from "../actions/post.action";
 
-const initialState = { post: "idir" };
+const initialState = {};
 
 export default function postReducer(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
       return action.payload;
+    case ADD_POST:
+      return [action.payload, ...state];
+    case EDIT_POST:
+      return state.map((post) => {
+        if (post.id === action.payload.id) {
+          return { ...post, content: action.payload.content };
+        } else return post;
+      });
     default:
       return state;
   }
