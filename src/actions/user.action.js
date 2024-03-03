222472db495d4afa9a85ec2bef989fb43db99adb
IdirX69 <adil.idirr@gmail.com>
2024-03-03 15:40:14 +0100

    user post

--- src/actions/user.action.js
+++ src/actions/user.action.js
@@ -3,7 +3,7 @@ export const GET_USER = "GET_USER";
 export const getUser = () => {
   return (dispatch) => {
     return axios.get("http://localhost:3000/user").then((res) => {
-      dispatch({ type: GET_USER, payload: res.data });
+      dispatch({ type: GET_USER, payload: res.data[0] });
     });
   };
 };
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const PostForm = () => {
+  const form = useRef();
+  const handleForm = async (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="form-container">
-      <form>
+      <form ref={form} onSubmit={(e) => handleForm(e)}>
         <input type="text" placeholder="Titre du poste" />
         <textarea placeholder="Postez vos pensées..."></textarea>
         <input type="submit" value="Envoyer" />
